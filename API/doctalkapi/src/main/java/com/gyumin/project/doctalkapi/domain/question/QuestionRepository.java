1332840090c8139a4b70e053bc751b5336614325
gyumin <pleasure082@gmail.com>
2020-03-12 16:31:30 +0900

    JpaRepository 추가

---
+++ API/doctalkapi/src/main/java/com/gyumin/project/doctalkapi/domain/question/QuestionRepository.java
@@ -0,0 +1,6 @@
+package com.gyumin.project.doctalkapi.domain.question;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
