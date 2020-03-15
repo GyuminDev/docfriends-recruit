4ad5cfee35f37da768b2cb57a77a7c9d9aeb8f5a
gyumin <pleasure082@gmail.com>
2020-03-16 06:17:26 +0900

    PS 주석 추가

--- PS/solution/solution.js
+++ PS/solution/solution.js
@@ -1,22 +1,27 @@
 const solution = (q) => {
+  // 문자열을 배열로 바꿈
   let array = q.split("");
 
   const number = [];
   let temp = '';
 
+  // 숫자인 경우 number 배열에 push
   array.forEach(ele => {
     if (!isNaN(ele)) {
+      // 숫자를 붙이기 위함
       temp += ele + '';
     } else {
-      if (temp) {
+      if (temp) { // 비어있지 않을 때만 push
         number.push(temp);
       }
       temp = '';
     }
   });
 
+  // 복사하여 정렬 - 원본 배열 바뀌지 않음.
   let sortedNumber = number.slice().sort((prev, next) =>  prev-next);
 
+  // 최대값, 최소값 index get
   const a = number.indexOf(sortedNumber[0]);
   const b = number.indexOf(sortedNumber[sortedNumber.length-1]);
 
